ccfee388175db59cf327584caf7dd080187baa80
lucsimao <lucasimao64@gmail.com>
2021-12-12 13:09:40 -0300

    feat: ensure call axios.get with url WHEN method is called

--- src/Client.ts
+++ src/Client.ts
@@ -1,5 +1,7 @@
+import axios from 'axios';
 export class Client {
   getBizEmailUsers(): unknown {
+    axios.get('https://jsonplaceholder.typicode.com/users');
     return { email: 'any_email.biz' };
   }
 }
