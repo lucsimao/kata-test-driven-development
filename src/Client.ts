3446c3df95109c60a9bc750dae66d0ed14449fce
lucsimao <lucasimao64@gmail.com>
2021-12-12 13:26:44 -0300

    feat: ensure return an empty array WHEN axios returns non empty array but with no .biz email

--- src/Client.ts
+++ src/Client.ts
@@ -1,9 +1,25 @@
 import axios from 'axios';
 export class Client {
   async getBizEmailUsers(): Promise<unknown> {
-    const result = await axios.get(
+    const { data } = await axios.get(
       'https://jsonplaceholder.typicode.com/users'
     );
+
+    const result = [];
+
+    for (let user of data) {
+      const email = user.email;
+
+      if (
+        email.charAt(email.length - 4) === '.' &&
+        email.charAt(email.length - 3) === 'b' &&
+        email.charAt(email.length - 2) === 'i' &&
+        email.charAt(email.length - 1) === 'z'
+      ) {
+        result.push(user.name);
+      }
+    }
+
     return result;
   }
 }
